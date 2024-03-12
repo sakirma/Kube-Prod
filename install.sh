7db5914c8e3b5ddcd9e9b418b8ed21bab25bd9d7
Huseyin Caliskan <14217365+sakirma@users.noreply.github.com>
2024-03-12 20:06:18 +0100

    feat: working proof virtual service for the pod test info

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ kind create cluster --config ./values.yaml
 
 echo 'Installing podinfo-istio helm chart'
 helm upgrade --install podinfo-istio ./charts/podinfo-istio -n podinfo-test --create-namespace
+
+kubectl label namespace podinfo-test istio-injection=enabled

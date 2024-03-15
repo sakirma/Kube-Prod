8283417f57d1a82bc690cce7482b4bcff4aff8cb
Huseyin Caliskan <14217365+sakirma@users.noreply.github.com>
2024-03-16 00:35:22 +0100

    WIP: implement istio ambient mesh

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+
+- Use Dev Space instead of skaffold?
+- Implement Istio ambient
+  - Would be nice to save lots of resources by not deploying any istio sidecars
+  - https://istio.io/latest/docs/ops/ambient/getting-started/ This got deployed easily.
+    Still would like to see if monitoring will work out of the box.
--- install.sh
+++ install.sh
@@ -2,13 +2,19 @@
 
 kind create cluster --config ./values.yaml
 
+
 ./scripts/cilium.sh
 (cd ./scripts/metallb && ./metalLB.sh)
+
+
+kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=444631bfe06f3bcca5d0eadf1857eac1d369421d" | kubectl apply -f -; }
+
 (cd ./scripts/istio && ./istio.sh)
 
 
 
 echo 'Installing podinfo-istio helm chart'
-helm upgrade --install podinfo-istio ./charts/podinfo-istio -n podinfo-test --create-namespace
-
+kubectl create namespace podinfo-test
 kubectl label namespace podinfo-test istio-injection=enabled
+helm upgrade --install podinfo-istio ./charts/podinfo-istio -n podinfo-test --create-namespace

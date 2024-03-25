6df117b1aaa45da98a41e804abc58ab8fb5d0b1d
Huseyin Caliskan <14217365+sakirma@users.noreply.github.com>
2024-03-25 16:09:48 +0100

    feat: add gateway & httproute for podinfo

--- install.sh
+++ install.sh
@@ -6,15 +6,10 @@ kind create cluster --config ./values.yaml
 ./scripts/cilium.sh
 (cd ./scripts/metallb && ./metalLB.sh)
 
-
-kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
-  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=444631bfe06f3bcca5d0eadf1857eac1d369421d" | kubectl apply -f -; }
-
 (cd ./scripts/istio && ./istio.sh)
-
 
 
 echo 'Installing podinfo-istio helm chart'
 kubectl create namespace podinfo-test
-kubectl label namespace podinfo-test istio-injection=enabled
-helm upgrade --install podinfo-istio ./charts/podinfo-istio -n podinfo-test --create-namespace
+kubectl label namespace podinfo-test istio.io/dataplane-mode=ambient
+helm upgrade --install podinfo ./charts/podinfo-istio -n podinfo-test --create-namespace
--- scripts/istio/istio.sh
+++ scripts/istio/istio.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
-helm repo add istio https://istio-release.storage.googleapis.com/charts
-helm repo update
+# Installing Istio Ambient which is alpha. This is not advisable to use for production since it is still in alpha stage.
+# However, this will be used for this project since it prevents me to deploy istio side cars which saves resources on my local machine.
 
 kubectl create namespace istio-system
 
-echo 'Installing istio cni'
-helm upgrade --install istio-cni istio/cni -n istio-system --wait
+kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=444631bfe06f3bcca5d0eadf1857eac1d369421d" | kubectl apply -f -; }
 
-echo 'Installing istio-base'
-helm upgrade --install istio-base istio/base -n istio-system \
-    --set defaultRevision=default
-
-echo 'Installing istiod'
-helm upgrade --install istiod istio/istiod --wait -n istio-system \
-    --set istio_cni.enabled=true \
-    --set istio_cni.chained=true
-
-echo 'Installing istio ingress'
-helm upgrade --install istio-ingressgateway istio/gateway --create-namespace -n istio-system -f ./ingress.yaml
+istioctl install --set profile=ambient --skip-confirmation
